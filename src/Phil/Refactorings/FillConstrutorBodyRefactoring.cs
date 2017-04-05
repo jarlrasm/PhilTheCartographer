3920ab66fbd00448b9a619c622aae6c9fe6167b8
Jarl Sveinung Flø Rasmussen <jarl@jarlandying.com>
2017-04-05 09:11:12 +0200

    Nullcheck

--- src/Phil/Refactorings/FillConstrutorBodyRefactoring.cs
+++ src/Phil/Refactorings/FillConstrutorBodyRefactoring.cs
@@ -39,7 +39,8 @@ namespace Phil.Refactorings
                 var semanticModel = (SemanticModel)await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
                 
                 var body = node.Body;
-                
+                if(body==null)
+                    return;
                 var properties = semanticModel.GetTypeSymbols(node)
                     .Where(x => x.Name != "this");
                 var unimplemntedProperties =
